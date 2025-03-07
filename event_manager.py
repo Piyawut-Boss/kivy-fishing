95a833e0a388aa183953b7a1e8f91e6f46878c76
ziczac <tanatan@Vindice.local>
2025-03-08 01:01:14 +0700

    เขียนอธิบายโค้ด event_manager.py

--- event_manager.py
+++ event_manager.py
@@ -1,19 +1,19 @@
-class EventManager:
-    def __init__(self):
-        self.callbacks = {
-            'fish_caught': [],
-            'hook_dropped': [],
-            'hook_retrieved': [],
-            'game_over': [],
-            'score_changed': [],
-            'boat_moved': []
+class EventManager: # สร้างคลาส EventManager สำหรับจัดการกับ event ต่างๆ
+    def __init__(self): # สร้างเมธอด __init__ ที่ไม่รับพารามิเตอร์
+        self.callbacks = { # ใช้เก็บค่าของ event และ แต่ละอีเวนต์จะมีลิสตืของ callback ที่จะถูกเรียกเมื่อมีการเกิดอีเวนต์นั้นๆ
+            'fish_caught': [], # สร้างลิสต์ของ callback ที่จะถูกเรียกเมื่อมีการเกิดอีเวนต์ ปลาถูกจับ
+            'hook_dropped': [], # สร้างลิสต์ของ callback ที่จะถูกเรียกเมื่อมีการเกิดอีเวนต์ ปล่อยเบ็ด
+            'hook_retrieved': [],   # สร้างลิสต์ของ callback ที่จะถูกเรียกเมื่อมีการเกิดอีเวนต์ ดึงเบ็ตขึ้นมา
+            'game_over': [], # สร้างลิสต์ของ callback ที่จะถูกเรียกเมื่อมีการเกิดอีเวนต์ เกมส์จบ
+            'score_changed': [], # สร้างลิสต์ของ callback ที่จะถูกเรียกเมื่อมีการเกิดอีเวนต์ คะแนนเปลี่ยน
+            'boat_moved': [] # สร้างลิสต์ของ callback ที่จะถูกเรียกเมื่อมีการเกิดอีเวนต์ เรือเคลื่อนที่
         }
         
-    def add_listener(self, event_name, callback):
-        if event_name in self.callbacks:
-            self.callbacks[event_name].append(callback)
+    def add_listener(self, event_name, callback): # สร้างเมธอด add_listener ที่รับพารามิเตอร์ event_name และ callback
+        if event_name in self.callbacks: # เช็คว่า event_name อยู่ใน self.callbacksมั้ย
+            self.callbacks[event_name].append(callback) # ถ้าใช่ ให้เพิ่ม callback ลงในลิสต์ของ event_name นั้นๆ
             
-    def trigger_event(self, event_name, *args):
-        if event_name in self.callbacks:
-            for callback in self.callbacks[event_name]:
-                callback(*args)
+    def trigger_event(self, event_name, *args): # สร้างเมธอด trigger_event ที่รับพารามิเตอร์ event_name และ args
+        if event_name in self.callbacks: # เช็คว่า event_name อยู่ใน self.callbacksมั้ย
+            for callback in self.callbacks[event_name]: # วนลูปเพื่อเรียก callback ทุกตัวในลิสต์ของ event_name นั้นๆ
+                callback(*args) # เรียก callback โดยส่ง args ไปด้วย
