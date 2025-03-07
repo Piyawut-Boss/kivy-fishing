611b0f0533c3493250f1fc16e08bd2ead62eaf7b
ziczac <tanatan@Vindice.local>
2025-03-08 00:08:00 +0700

    อธิบายโค้ด config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 class GameConfig:
     WINDOW_SIZE = (1080, 720)
-    FISH_COUNT = 5
-    HOOK_SPEED = 4
-    BOAT_SPEED = 5
-    FISH_SPEED = 2
-    MAX_DEPTH = 700
+    FISH_COUNT = 5 #จำนวนปลา
+    HOOK_SPEED = 4 #ความเร็วของเบ็ตตกปลา
+    BOAT_SPEED = 5 #ความเร็วของเรือ
+    FISH_SPEED = 2 #ความเร็วของปลา
+    MAX_DEPTH = 700 #ความยาวของเบ็ต
