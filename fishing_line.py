1c36c3f155db25bba0ef600d29bf1ee01728413a
ziczac <tanatan@Vindice.local>
2025-03-08 15:11:38 +0700

    เขียนอธิบายโค้ด fishing_line.py

--- fishing_line.py
+++ fishing_line.py
@@ -1,23 +1,23 @@
-from boat import Boat
+from boat import Boat # นำเข้าโมดูล Boat จากไฟล์ boat.py
 
-class FishLine:
+class FishLine: # สร้างคลาส FishLine สำหรับจัดการสายเบ็ดตกปลา
 
-    def __init__(self, boat: Boat):
+    def __init__(self, boat: Boat): # กำหนดพารามิเตอร์ boat เป็นอ็อบเจกต์ Boat
         self.boat = boat  # เก็บอ้างอิงถึงเรือ
         self.update_tip_of_the_rod()  # ตั้งค่าปลายคันเบ็ดตามทิศทางเรือ
-        self.advance_line = 150
+        self.advance_line = 150 # ความยาวของสายเบ็ด
 
-    def update_tip_of_the_rod(self):
+    def update_tip_of_the_rod(self): # สร้างเมธอด update_tip_of_the_rod สำหรับอัปเดตตำแหน่งปลายคันเบ็ด
         """อัปเดตตำแหน่งปลายคันเบ็ดตามทิศทางเรือ"""
-        if self.boat.direction == "right":
-            self.tip_of_the_rod = self.boat.x + 210
+        if self.boat.direction == "right": #ถ้าเรือไปทางขวา
+            self.tip_of_the_rod = self.boat.x + 210 # ตำแหน่งปลายคันเบ็ดอยู่ทางขวาของเรือ
         else:
-            self.tip_of_the_rod = self.boat.x + 35
+            self.tip_of_the_rod = self.boat.x + 35 # ตำแหน่งปลายคันเบ็ดอยู่ทางซ้ายของเรือ
 
-    def rotate_fisherman_right(self):
-        self.boat.direction = "right"
-        self.update_tip_of_the_rod()
+    def rotate_fisherman_right(self): # สร้างเมธอด rotate_fisherman_right สำหรับหมุนตัวลูกเบ็ดไปทางขวา
+        self.boat.direction = "right" # ตั้งค่าทิศทางของเรือเป็นขวา
+        self.update_tip_of_the_rod() # อัปเดตตำแหน่งปลายคันเบ็ด
 
-    def rotate_fisherman_left(self):
-        self.boat.direction = "left"
-        self.update_tip_of_the_rod()
+    def rotate_fisherman_left(self): # สร้างเมธอด rotate_fisherman_left สำหรับหมุนตัวลูกเบ็ดไปทางซ้าย
+        self.boat.direction = "left" # ตั้งค่าทิศทางของเรือเป็นซ้าย
+        self.update_tip_of_the_rod() # อัปเดตตำแหน่งปลายคันเบ็ด
