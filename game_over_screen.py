3a2ee859627c897da24473f89326462cc8a14fe0
Piyawut-Boss <6710110264@psu.ac.th>
2025-03-09 23:42:45 +0700

    add quit game button

--- game_over_screen.py
+++ game_over_screen.py
@@ -2,6 +2,8 @@ from kivy.uix.screenmanager import Screen
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.uix.label import Label
+from kivy.core.window import Window
+from kivy.app import App
 
 class GameOverScreen(Screen):
     def __init__(self, **kwargs):
@@ -44,10 +46,23 @@ class GameOverScreen(Screen):
         )
         menu_btn.bind(on_release=self.go_to_menu)
         
+        # Add Quit button
+        quit_btn = Button(
+            text='Quit Game',
+            size_hint=(None, None),
+            size=(300, 80),
+            pos_hint={'center_x': 0.5},
+            font_size=40,
+            background_color=(1, 0.3, 0.3, 1)  # Red color
+        )
+        quit_btn.bind(on_release=self.quit_game)
+        
         layout.add_widget(game_over_label)
         layout.add_widget(self.score_label)
         layout.add_widget(retry_btn)
         layout.add_widget(menu_btn)
+        layout.add_widget(quit_btn)  # Add quit button at the end
+        
         self.add_widget(layout)
     
     def update_score(self, score):
@@ -58,3 +73,7 @@ class GameOverScreen(Screen):
     
     def go_to_menu(self, *args):
         self.manager.current = 'menu'
+    
+    def quit_game(self, *args):
+        """Close the entire application"""
+        App.get_running_app().stop()
